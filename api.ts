d9f99eaeceb6cbf97e72661055d70d9c096e8ba5
Jeffrey <jeffrey@jeffreyzutt.nl>
2023-10-24 15:17:57 +0200

    decrease sleepy time

--- api.ts
+++ api.ts
@@ -2,19 +2,12 @@ import { Product } from "./types";
 
 const urlBase = "https://frasers-workshop-api.vercel.app";
 
-const sleep = (timeout: number) =>
-  new Promise((resolve) => setTimeout(resolve, timeout));
-
 const api = {
   product: {
     list: async (): Promise<Product[]> =>
       fetch(`${urlBase}/api/products`).then((res) => res.json()),
-    fetch: async (handle: string): Promise<Product> => {
-      await sleep(5000); // Sleep to see the streaming in action
-      return fetch(`${urlBase}/api/products/${handle}`).then((res) =>
-        res.json(),
-      );
-    },
+    fetch: async (handle: string): Promise<Product> =>
+      fetch(`${urlBase}/api/products/${handle}`).then((res) => res.json()),
   },
 };
 
--- app/AddToCart.tsx
+++ app/AddToCart.tsx
@@ -5,7 +5,11 @@ type Props = {
   handle: string;
 };
 
+const sleep = (timeout: number) =>
+  new Promise((resolve) => setTimeout(resolve, timeout));
+
 export const AddToCart = async ({ handle }: Props) => {
+  await sleep(2000); // Sleep to see the streaming in action
   const productData = await api.product.fetch(handle);
   return <AddToCartComponent inStock={productData.inStock} />;
 };
